fc0d9d7e3569cc6165ccdd48fec2213f85df4667
Loreleï Corvaisier <loreleicvs@hotmail.com>
2024-10-07 15:54:03 +0200

    premières questions

---
+++ script.py
@@ -0,0 +1,34 @@
+import requests
+from pprint import pprint
+import time 
+
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
+    }
+url = "https://www.bandsintown.com/choose-dates/fetch-next/upcomingEvents"
+
+fin = []
+for i in range(25):
+    params = {
+        "city_id": 3117735,
+        "date": "2024-10-01T00:00:00,2024-10-31T23:00:00",
+        "page": i,  # Vous pouvez modifier cette valeur pour naviguer à travers les pages
+        "longitude": -3.70256,
+        "latitude": 40.4165,
+        "genre_query": "all-genres"
+    }
+    time.sleep(1)
+    response = requests.get(url, params=params, headers=headers).json()
+    data = response['events']
+    if len(data)>0 :
+        fin.extend(data)
+    else:
+        print("plus d'info",i)
+        break
+pprint(fin)
+
+def save_json(response,idx_page):
+    with open('sauvegarde_json/response_data_{idx_page}.json', 'w', encoding='utf-8') as json_file:
+            json.dump(response, json_file)
+        

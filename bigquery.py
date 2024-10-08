5019a5169b6de99bd3d33ef1a077ba09d1d83b7d
Manon240 <manon.cousin02@gmail.com>
2024-10-08 16:47:31 +0200

    Code BigQuery pour déposer df sur GCP

---
+++ bigquery.py
@@ -0,0 +1,14 @@
+from google.oauth2 import service_account
+import pandas_gbq
+import pandas as pd
+
+df = pd.read_csv("df_event_base.csv")
+credentials = service_account.Credentials.from_service_account_file(
+    "sa-key-group-5.json",
+)
+pandas_gbq.to_gbq(
+    df,
+    "dataset_group_5.my_table",
+    project_id="ai-technologies-ur2",
+    credentials=credentials,
+)

4fec1c2adc1e88e6d1e1f639c75432d5714d3f46
Juliette Brault <juliettebrault03@gmail.com>
2025-10-17 11:33:14 +0200

    enrichissement info artiste

--- enrichissement_info.py
+++ enrichissement_info.py
@@ -6,70 +6,55 @@ import time
 import random
 from tqdm import tqdm
 import re
+import json
 
 df = load_all_events()
-df = df.iloc[1100:]
+df = df.head()
 
 headers = {
     "User-Agent": "Mozilla/5.0" 
 }
 
-def extract_hometown(soup):
-    for div in soup.find_all("div"):
-        if div.text.strip() == "Hometown:":
-            next_div = div.find_next_sibling("div")
-            if next_div:
-                return next_div.text.strip()
-    return None
 
-def extract_genres(soup):
+def extract_label(soup, label):
     for div in soup.find_all("div"):
-        if div.text.strip() == "Genres:":
+        if div.text.strip() == label:
             next_div = div.find_next_sibling("div")
             if next_div:
                 return next_div.text.strip()
-    return None
+
+
+def extract_followers(soup):
+    for div in soup.find_all("div"):
+        text = div.text.strip()
+        if text.lower().endswith("followers"):
+            num = re.sub(r"[^\d]", "", text) 
+            if num:
+                return int(num)
 
 
 
 def get_artist_info(artist_url):
     response = requests.get(artist_url, headers= headers)
     soup = BeautifulSoup(response.text, "html.parser")
-
-    # Hometown
-    hometown = extract_hometown(soup)
-
-    # Followers
-    followers = None
-    for div in soup.find_all("div"):
-        text = div.text.strip()
-        if text.lower().endswith("followers"):
-            num = re.sub(r"[^\d]", "", text)  # garde que les chiffres
-            followers = int(num) if num else None
-            break
-
-    # Genres 
-    genres = extract_genres(soup)
-
+    
     return {
-        "hometown": hometown,
-        "followers": followers,
-        "genres": genres
+        "hometown": extract_label(soup, "Hometown:"),
+        "genres": extract_label(soup, "Genres:"),
+        "followers": extract_followers(soup)  
     }
+
+
 
 result = []
 for url in tqdm(df['artistUrl']):
     info = get_artist_info(url)
     result.append(info)
-    time.sleep(random.uniform(1, 1))
+    time.sleep(1)
 
 info_df = pd.DataFrame(result)
-
 df_new = pd.concat([df.reset_index(drop = True), info_df], axis = 1)
 
-json_output = df_new.to_json(orient="records", indent=4)
-print(json_output.replace("\\/", "/"))
-
-# Optionnel : sauvegarde dans un fichier
-with open("data_info/events_enriched23.json", "w", encoding="utf-8") as f:
-    f.write(json_output.replace("\\/", "/"))
+json_output = df_new.to_json(orient="records", indent=4).replace("\\/", "/")
+with open("data_info/events_test.json", "w", encoding="utf-8") as f:
+    f.write(json_output)
